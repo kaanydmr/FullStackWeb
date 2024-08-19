39ac19b4faa032f2eaee49689fc02b72df4b6f4e
kaanydmr <118356856+kaanydmr@users.noreply.github.com>
2024-08-19 11:15:05 +0300

    Update to send data part part

--- Get_Data/main.py
+++ Get_Data/main.py
@@ -19,12 +19,13 @@ driver = webdriver.Remote(
     command_executor='http://selenium:4444/wd/hub',
     options=options,
 )
-
+min_age = 15
+mid_age = 40
+max_age = 100
 url = "https://www.interpol.int/How-we-work/Notices/Red-Notices/View-Red-Notices"
 driver.get(url)
 
-unique_ids = set()  # To store unique entity IDs
-notice_list = []
+unique_ids = set()
 
 nati = []
 for i in range(2, 218):
@@ -32,10 +33,67 @@ for i in range(2, 218):
     nation_option = wait.until(EC.element_to_be_clickable((By.XPATH, f"/html/body/div[1]/div[1]/div[6]/div/div[1]/div/div/form/div[3]/select/option[{i}]")))
     nati.append(nation_option.get_attribute("value"))
 
+# Connection for RabbitMQ
+connection_parameters = pika.ConnectionParameters('host.docker.internal')
+connection = pika.BlockingConnection(connection_parameters)
+channel = connection.channel()
+
+channel.queue_declare(queue='interpol_notices')
+
+def searchBy_arrestwarrantcountry(gender, nations, min_age, mid_age, max_age):
+    for nationality in nations:
+        url = f"https://ws-public.interpol.int/notices/v1/red?arrestWarrantCountryId={nationality}&resultPerPage=160"
+        total = process_notices(url)
+
+        if total > 160:
+            url = f"https://ws-public.interpol.int/notices/v1/red?arrestWarrantCountryId={nationality}&ageMin={min_age}&ageMax={mid_age}&sexId={gender}&resultPerPage=160&page=1"
+            total = process_notices(url)
+
+            if total > 160:
+                for age in range(min_age, mid_age):
+                    url = f"https://ws-public.interpol.int/notices/v1/red?arrestWarrantCountryId={nationality}&ageMin={age}&ageMax={age}&sexId={gender}&resultPerPage=160&page=1"
+                    total = process_notices(url)
+
+            url = f"https://ws-public.interpol.int/notices/v1/red?arrestWarrantCountryId={nationality}&ageMin={mid_age}&ageMax={max_age}&sexId={gender}&resultPerPage=160&page=1"
+            total = process_notices(url)
+
+            if total > 160:
+                for age in range(mid_age, max_age):
+                    url = f"https://ws-public.interpol.int/notices/v1/red?arrestWarrantCountryId={nationality}&ageMin={age}&ageMax={age}&sexId={gender}&resultPerPage=160&page=1"
+                    total = process_notices(url)
+
+def searchBy_nationality(gender, nations, min_age, mid_age, max_age):
+    for nationality in nations:
+        url = f"https://ws-public.interpol.int/notices/v1/red?nationality={nationality}&sexId={gender}&resultPerPage=160"
+        total = process_notices(url)
+        if total > 160:
+            url = f"https://ws-public.interpol.int/notices/v1/red?nationality={nationality}&ageMin={min_age}&ageMax={mid_age}&sexId={gender}&resultPerPage=160&page=1"
+            total = process_notices(url)
+
+            if total > 160:
+                for age in range(min_age, mid_age):
+                    url = f"https://ws-public.interpol.int/notices/v1/red?nationality={nationality}&ageMin={age}&ageMax={age}&sexId={gender}&resultPerPage=160&page=1"
+                    total = process_notices(url)
+
+            url = f"https://ws-public.interpol.int/notices/v1/red?nationality={nationality}&ageMin={mid_age}&ageMax={max_age}&sexId={gender}&resultPerPage=160&page=1"
+            total = process_notices(url)
+
+            if total > 160:
+                for age in range(mid_age, max_age):
+                    url = f"https://ws-public.interpol.int/notices/v1/red?nationality={nationality}&ageMin={age}&ageMax={age}&sexId={gender}&resultPerPage=160&page=1"
+                    total = process_notices(url)
+
+def search_notices(nations, min_age, mid_age, max_age):
+    for gender in ['F', 'M']:
+        searchBy_arrestwarrantcountry(gender, nations, min_age, mid_age, max_age)
+        searchBy_nationality(gender, nations,min_age, mid_age, max_age)
+
 def process_data(notice):
     if notice:
-        notice_list.append(notice)
-        print(f"Notice {notice['entity_id']} added to list")
+        notice_dict = {"notices": [notice]}
+        message_body = json.dumps(notice_dict)
+        channel.basic_publish(exchange='', routing_key='interpol_notices', body=message_body)
+        print(f"Notice {notice['entity_id']} sent to RabbitMQ")
 
 def process_notices(url):
     driver.get(url)
@@ -63,80 +121,10 @@ def process_notices(url):
         print("Failed to parse JSON data.")
         return 0
 
-
-#For people that has Unkown gender
+# For people that have Unknown gender
 url = f"https://ws-public.interpol.int/notices/v1/red?&sexId=U&resultPerPage=160"
 total = process_notices(url)
+search_notices(nati, min_age, mid_age, max_age)
 
-#Loop for finding people with parameters: Gender and what country is issued warrant with age as data per page is 160 notice max
-for gender in ['F', 'M']:
-    for nationality in nati:
-        # Check the initial total
-        url = f"https://ws-public.interpol.int/notices/v1/red?arrestWarrantCountryId={nationality}&resultPerPage=160"
-        total = process_notices(url)
-
-        # If total exceeds 160, fetch data for different age ranges
-        if total > 160:
-
-            url = f"https://ws-public.interpol.int/notices/v1/red?arrestWarrantCountryId={nationality}&ageMin={10}&ageMax={40}&sexId={gender}&resultPerPage=160&page=1"
-            total = process_notices(url)
-
-            if total > 160:
-                for age in range(10, 40):
-                    url = f"https://ws-public.interpol.int/notices/v1/red?arrestWarrantCountryId={nationality}&ageMin={age}&ageMax={age}&sexId={gender}&resultPerPage=160&page=1"
-                    total = process_notices(url)
-
-
-            url = f"https://ws-public.interpol.int/notices/v1/red?arrestWarrantCountryId={nationality}&ageMin={40}&ageMax={100}&sexId={gender}&resultPerPage=160&page=1"
-            total = process_notices(url)
-
-            if total > 160:
-                for age in range(40, 100):
-                    url = f"https://ws-public.interpol.int/notices/v1/red?arrestWarrantCountryId={nationality}&ageMin={age}&ageMax={age}&sexId={gender}&resultPerPage=160&page=1"
-                    total = process_notices(url)
-
-
-#Loop for finding people with parameters: Gender and what country they are from with age
-for gender in ['F', 'M']:
-    for nationality in nati:
-        # Check the initial total
-        url = f"https://ws-public.interpol.int/notices/v1/red?nationality={nationality}&sexId={gender}&resultPerPage=160"
-        total = process_notices(url)
-
-        if total > 160:
-            # If total exceeds 160, fetch data for different age ranges
-            url = f"https://ws-public.interpol.int/notices/v1/red?nationality={nationality}&ageMin={10}&ageMax={40}&sexId={gender}&resultPerPage=160&page=1"
-            total = process_notices(url)
-
-            if total > 160:
-                for age in range(10, 40):
-                    url = f"https://ws-public.interpol.int/notices/v1/red?nationality={nationality}&ageMin={age}&ageMax={age}&sexId={gender}&resultPerPage=160&page=1"
-                    total = process_notices(url)
-
-            url = f"https://ws-public.interpol.int/notices/v1/red?nationality={nationality}&ageMin={40}&ageMax={100}&sexId={gender}&resultPerPage=160&page=1"
-            total = process_notices(url)
-
-            if total > 160:
-                for age in range(40, 100):
-                    url = f"https://ws-public.interpol.int/notices/v1/red?nationality={nationality}&ageMin={age}&ageMax={age}&sexId={gender}&resultPerPage=160&page=1"
-                    total = process_notices(url)
-
-# Close the WebDriver after all nationalities are processed
 driver.quit()
-
-#Connection for RabbitMQ
-connection_parameters = pika.ConnectionParameters('host.docker.internal')
-connection = pika.BlockingConnection(connection_parameters)
-channel = connection.channel()
-
-
-channel.queue_declare(queue='interpol_notices')
-
-notice_dict = {"notices": notice_list}
-
-message_body = json.dumps(notice_dict)
-
-# Send the JSON string as the message body to RabbitMQ
-channel.basic_publish(exchange='', routing_key='interpol_notices', body=message_body)
-
 connection.close()
